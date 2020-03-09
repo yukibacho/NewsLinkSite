d980354c9b932e1d5aac9cd8a16792955e686619
yukibacho <57794992+yukibacho@users.noreply.github.com>
2020-03-09 13:30:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import url_for
 from flask import render_template
 from flask import jsonify
 from flask import request
+from flask import make_response
+from io import StringIO
+import csv
 
 from DataStore.MySQLClient import MySQLClient
 import news_scraping
@@ -26,24 +29,60 @@ app = Flask(__name__)
 def main():
     props = {'title': 'News Topics', 'msg': 'News Topics'}
     stmf = 'SELECT * FROM NEWS_TBL ORDER BY ID ASC'
-    topics = db.query(stmf)
-    f = open('updatetime.txt')
-    time = f.read();
-    html = render_template('index.html', props=props, topics=topics, time=time)
+    try:
+        topics = db.query(stmf)
+    except:
+        errortype = "DB"
+        return
+    else:
+        f = open('updatetime.txt')
+        time = f.read();
+        html = render_template('index.html', props=props, topics=topics, time=time)
+        return html
+
+@app.route("/error")
+def error(errortype):
+    props = {'title': 'ERROR', 'msg': errortype + 'エラーです。'}
+    html = render_template('error.html',props=props)
     return html
 
 @app.route('/api/udnews/<user>/<password>', methods=["POST","GET"])
 def update_news(user,password):
-    news_scraping.getnews(user,password)
-    return redirect(url_for('main'))
+    try:
+        news_scraping.getnews(user,password)
+    except:
+        errortype = "proxy認証"
+        return redirect(url_for('error'))
+    else:
+        return redirect(url_for('main'))
+
+@app.route('/export')
+def export_news():
+    ft = open('updatetime.txt')
+    time = ft.read();
+    reptime = time.replace(' ','_')
+    f = StringIO()
+    writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+    stmf = 'SELECT * FROM NEWS_TBL ORDER BY ID ASC'
+    topics = db.query(stmf)
+    for t in topics:
+        writer.writerow([t[0],t[1],t[2]])
+
+    res = make_response()
+    res.data = f.getvalue()
+    res.headers['Content-Type'] = 'text/csv'
+    res.headers['Content-Disposition'] = 'attachment; filename=news_' + reptime +'.csv'
+    return res
+
 
 @app.route('/api/news', methods=["GET"])
 def all_news_get():
     stmf = 'SELECT * FROM NEWS_TBL ORDER BY ID ASC'
     topics = db.query(stmf)
+
     topicsj = [{
         'ID':topic[0],
-        'title':topic[1],
+        'title':topic[1],   #.decode('utf-8').encode().decode('unicode-escape'),
         'link':topic[2]
     }for topic in topics]
     json = {
@@ -52,7 +91,7 @@ def all_news_get():
     return jsonify(json)
 
 @app.route('/api/news/<int:id>', methods=["GET"])
-def news_get():
+def news_get(id):
     stmf = 'SELECT * FROM NEWS_TBL WHERE ID =' + str(id)
     topic = db.query(stmf)
     json = {
--- news_scraping.py
+++ news_scraping.py
@@ -16,10 +16,6 @@ def getnews(user,password):
 	}
 	db = MySQLClient(**dns)
 
-	# NEWS_TBLのレコード情報を削除
-	stmt = 'TRUNCATE TABLE NEWS_TBL'
-	db.addquery(stmt)
-
 	# 認証プロキシを指定
 	user = user
 	pw = password
@@ -29,22 +25,29 @@ def getnews(user,password):
 	os.environ["http_proxy"] = proxyurl
 	os.environ["https_proxy"] = proxyurl
 
-	# yahooニュースからのwebスクレイピング
-	r = requests.get("https://news.yahoo.co.jp/",verify=False)
-	soup = BeautifulSoup(r.content, "html.parser")
-
-	#主要ニューストピック情報を取得
-	topics = soup.select('.topicsListItem')
-
-	i = 1
-	for topic in topics:
-		stmt = 'INSERT INTO NEWS_TBL VALUES (' + str(i) + ",'" + topic.next.next + "','" + topic.next.attrs.get("href") + "')"
+	try:
+		# yahooニュースからのwebスクレイピング
+		r = requests.get("https://news.yahoo.co.jp/",verify=False)
+	except requests.exceptions.ProxyError:
+		pass
+	else:
+		# NEWS_TBLのレコード情報を削除
+		stmt = 'TRUNCATE TABLE NEWS_TBL'
 		db.addquery(stmt)
-		i = i + 1
+		soup = BeautifulSoup(r.content, "html.parser")
 
-	file = open('updatetime.txt', 'w')
-	file.write(str(datetime.datetime.now()))
-	file.close()
+		#主要ニューストピック情報を取得
+		topics = soup.select('.topicsListItem')
 
+		i = 1
+		for topic in topics:
+			stmt = 'INSERT INTO NEWS_TBL VALUES (' + str(i) + ",'" + topic.next.next + "','" + topic.next.attrs.get("href") + "')"
+			db.addquery(stmt)
+			i = i + 1
+
+		file = open('updatetime.txt', 'w')
+		file.write(str(datetime.datetime.now()))
+		file.close()
+		
 if __name__ == '__main__':
 	getnews()
